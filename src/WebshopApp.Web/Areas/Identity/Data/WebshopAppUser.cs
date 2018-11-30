5b49e8db5c1b1cd21bcd7fccb3d80a99d76c1462
Borislav Enchev <thugluv@abv.bg>
2018-11-30 11:29:06 +0200

    Scaffolded identity

---
+++ src/WebshopApp.Web/Areas/Identity/Data/WebshopAppUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace WebshopApp.Web.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the WebshopAppUser class
+    public class WebshopAppUser : IdentityUser
+    {
+    }
+}
