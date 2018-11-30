31b7b408cc0911ba8fa2e7564dada1c722c3ecf6
Borislav Enchev <thugluv@abv.bg>
2018-11-30 11:59:17 +0200

    Adding missing files to the previous commit

---
+++ src/WebshopApp.Models/WebshopAppUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebshopApp.Models
+{
+    // Add profile data for application users by adding properties to the WebshopAppUser class
+    public class WebshopAppUser : IdentityUser
+    {
+
+    }
+}
