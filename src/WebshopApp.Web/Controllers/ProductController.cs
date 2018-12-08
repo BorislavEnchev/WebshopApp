f94835ea1a612615c04829e0638d399e79860e4b
Borislav Enchev <thugluv@abv.bg>
2018-12-08 14:48:04 +0200

    Product creation introduced. (Methods implemented and View created)

--- src/WebshopApp.Web/Controllers/ProductController.cs
+++ src/WebshopApp.Web/Controllers/ProductController.cs
@@ -1,5 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using WebshopApp.Services.DataServices;
 using WebshopApp.Services.DataServices.Contracts;
 using WebshopApp.Services.Models;
@@ -10,10 +13,12 @@ namespace WebshopApp.Web.Controllers
     public class ProductController : BaseController
     {
         private readonly IProductsService productsService;
+        private readonly ICategoriesService categoriesService;
 
-        public ProductController(IProductsService productsService)
+        public ProductController(IProductsService productsService, ICategoriesService categoriesService)
         {
             this.productsService = productsService;
+            this.categoriesService = categoriesService;
         }
 
         public IActionResult Details(int id)
@@ -21,6 +26,18 @@ namespace WebshopApp.Web.Controllers
             var product = this.productsService.GetProductById<ProductViewModel>(id);
 
             return this.View(product);
+        }
+
+        [Authorize]
+        public IActionResult Create()
+        {
+            this.ViewData["Categories"] = this.categoriesService.GetAll()
+                .Select(x => new SelectListItem
+                {
+                    Value = x.Id.ToString(),
+                    Text = x.Name,
+                });
+            return this.View();
         }
 
         [HttpPost]
