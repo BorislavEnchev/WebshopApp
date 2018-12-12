4dec1cd4a21b79ce6def5eb5be66944e19a76816
Borislav Enchev <thugluv@abv.bg>
2018-12-12 17:05:27 +0200

    ImageController.cs introduced. ListAllProductFromCategory View introduced.

---
+++ src/WebshopApp.Web/Controllers/CategoryController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WebshopApp.Services.DataServices.Contracts;
+using WebshopApp.Services.Models;
+
+namespace WebshopApp.Web.Controllers
+{
+    public class CategoryController : BaseController
+    {
+        private readonly ICategoriesService categoriesService;
+
+        public CategoryController(ICategoriesService categoriesService, IProductsService productsService)
+        {
+            this.categoriesService = categoriesService;
+        }
+
+        public IActionResult ListAllProductFromCategory(int id)
+        {
+            this.ViewData["Categories"] = this.categoriesService.GetAll()
+                .Select(x => new CategoryViewModel
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                }).ToList();
+
+            if (ViewData["Categories"] == null)
+            {
+                throw new ArgumentException("Categories are empty");
+            }
+
+            var products = this.categoriesService.GetAllProductsFromCategory(id).ToList();
+
+            return this.View(products);
+        }
+    }
+}
