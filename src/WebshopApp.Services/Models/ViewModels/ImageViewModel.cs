2a1b9e7a0c535bc07765476e62516233302b7297
Borislav Enchev <thugluv@abv.bg>
2018-12-25 10:27:04 +0200

    ImageViewModel.cs and _ProductPartial.cshtml Introduced

---
+++ src/WebshopApp.Services/Models/ViewModels/ImageViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace WebshopApp.Services.Models.ViewModels
+{
+    public class ImageViewModel
+    {
+        public int ProductId { get; set; }
+
+        public IFormFile Image { get; set; }
+    }
+}
