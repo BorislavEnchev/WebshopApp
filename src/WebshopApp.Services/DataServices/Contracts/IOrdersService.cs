4b8deafe9a5d827b00bb3b6ba9d8b3086f7aebe0
Borislav Enchev <thugluv@abv.bg>
2019-01-03 11:24:49 +0200

    OrdersService introduced with the interface.

---
+++ src/WebshopApp.Services/DataServices/Contracts/IOrdersService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using WebshopApp.Services.Models.ViewModels;
+
+namespace WebshopApp.Services.DataServices.Contracts
+{
+    public interface IOrdersService
+    {
+        Task<string> Create(ShoppingCartViewModel model, string userId);
+    }
+}
---
+++ src/WebshopApp.Services/DataServices/OrdersService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using WebshopApp.Data.Common;
+using WebshopApp.Models;
+using WebshopApp.Services.DataServices.Contracts;
+using WebshopApp.Services.Models.ViewModels;
+
+namespace WebshopApp.Services.DataServices
+{
+    public class OrdersService : IOrdersService
+    {
+        private readonly IRepository<Order> orderRepository;
+        private readonly IRepository<Product> productRepository;
+
+        public OrdersService(IRepository<Order> orderRepository, IRepository<Product> productRepository)
+        {
+            this.orderRepository = orderRepository;
+            this.productRepository = productRepository;
+        }
+
+        public async Task<string> Create(ShoppingCartViewModel model, string userId = null)
+        {
+            var order = new Order
+            {
+                Id = Guid.NewGuid().ToString(),
+                Products = model.Products,
+                ClientId = userId
+            };
+
+            await this.orderRepository.AddAsync(order);
+            await this.orderRepository.SaveChangesAsync();
+
+            foreach (var product in model.Products)
+            {
+                product.Unit -= product.Quantity;
+            }
+
+            this.productRepository.UpdateRange(model.Products);
+            await this.productRepository.SaveChangesAsync();
+
+            return order.Id;
+        }
+    }
+}
